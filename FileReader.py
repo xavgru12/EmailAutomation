793eaf3f3f2d380289ee71d3b32755d2729c7550
Xaver Gruber <xaver.max.gruber@gmail.com>
2022-11-12 16:39:40 +0100

    add logic for file parser of content, subject and greeting

--- FileReader.py
+++ FileReader.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
+from ModelData import ModelData
 
 
 def main():
     fileReader = FileReader("CatchUp")
     print(fileReader.context)
-    data=fileReader.getFormalGreeting("./Model/General/GreetingFormal.txt")
-    print(data)
+    #data=fileReader.getFormalGreeting("./Model/General/GreetingFormal.txt")
+    #print(data)
+    fileReader.createModelDataObject()
 
 
 
@@ -19,6 +21,16 @@ class FileReader:
         contentDict = self.getContent("./Model/"+self.context+"/Content.txt")
         subjectDict = self.getSubject("./Model/"+self.context+"/Subject.txt")
 
+        modelData = ModelData(contentDict, subjectDict, formalGreetingDict,  informalGreetingDict)
+
+        print(formalGreetingDict)
+        print(informalGreetingDict)
+        print(contentDict)
+        print(subjectDict)
+
+        return modelData
+
+
     def getFormalGreeting(self, filepath):
         fileAsDict= self.parseFile(filepath)
         return fileAsDict
@@ -28,47 +40,39 @@ class FileReader:
         return fileAsDict
 
     def getContent(self, filepath):
-        fileAsDict= self.parseFile(filepath)
+        fileAsDict= self.parseFile(filepath, "")
         return fileAsDict
 
     def getSubject(self, filepath):
         fileAsDict= self.parseFile(filepath)
         return fileAsDict
 
-    def parseFile(self, filepath):
+    def parseFile(self, filepath, bufferstrip="\n"):
         content={}
         with open(filepath, mode="r") as file:
             buffer=""
             name=""
-            for line in file: #really complicated logic, try with yield and next() to split up logic
-                if '#' in line:
-                    if buffer == "":
-                        name=line.rstrip('\n').lstrip('#')
-                        continue
-                    if name:
-                        content[name]=buffer
-                        buffer=""
-                    name=line.rstrip('\n').lstrip('#')
-                    continue
-                buffer+=line.rstrip('\n')
+
+            for line in file: 
+                markerLine=self.checkIfMarkerLine(line, '#')
+
+                if not markerLine:
+                    buffer+=line.rstrip(bufferstrip)    #add line to buffer as it is no markerLine with '#'
+                elif markerLine:
+                    if name != "":                      #only add data to dictionary if name is not empty, eg at first markerLine in file
+                        content[name]=buffer            # add buffer content to dict
+                        buffer=""                       #reset buffer after it is put in content dictionary
+                    name=line.rstrip('\n').lstrip('#')  #save name which will be added in dict next iteration
+
+            content[name]=buffer                        #add last data in dict as for loop ended and no next iteration of loop available
 
         return content
 
-"""
-                    #buffer+=line
-                else=
-            while "#" not in file:
-                value = next(file).rstrip('\n')
-                buffer+=value
-            yield buffer
-  
-            next(file); next(file)  # ignore first two lines
-            value = next(file).rstrip('\n')  # read what should be the first number
-            while "#" not in value:#value != '#extra':  # not end-of-numbers marker
-                buffer+=value
-                yield value
-                value = next(file).rstrip('\n')"""
-
+    def checkIfMarkerLine(self, line, marker):
+        if marker in line:
+            return True
+        else: 
+            return False
 
 
 
